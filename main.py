7bd1e0e55f06b39581ece0c0e0b0415ab5eb6637
Anvay Vahia <110357337+CyberLord09@users.noreply.github.com>
2025-06-24 16:08:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -20,4 +20,5 @@ def get_scores():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 8080))  # REQUIRED for Railway
+    app.run(host="0.0.0.0", port=port)
